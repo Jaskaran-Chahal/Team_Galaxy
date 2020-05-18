047b9fc9f8f7ea51f57710ac76bbc53065bdb66c
Jaskaran-Chahal <jaskaran.s.chahal@gmail.com>
2020-05-17 21:51:14 -0700

    Added splash screen towards of application

---
+++ app/src/main/java/info/jakelee/spacewars/SplashActivity.java
@@ -0,0 +1,26 @@
+package info.jakelee.spacewars;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        long delayMillis = 4*1000;//4 seconds delay
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),Spacewar.class));
+            }
+
+
+        }, delayMillis);
+    }
+}
